a2c0adc6bace63e2b2917683310d132effd04aa1
Slinkter <luis.j.cueva@gmail.com>
2024-04-17 18:28:14 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
-import { useState, useEffect, useRef } from "react";
-import React from "react";
 import Routes from "./Routes/Index";
-
-import Navbar from "./Components/Navbar";
-import Events from "./Components/Events";
-
 import "./App.css";
 
 function App() {
